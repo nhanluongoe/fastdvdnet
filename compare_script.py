451a66a317581aff058f773bbc0167b0a5e052e3
Nhan Luong <nhanlt18@gmail.com>
2020-12-24 12:16:35 +0700

    added concat before and after

---
+++ compare_script.py
@@ -0,0 +1,28 @@
+import cv2
+import os
+
+# load images into a list and sort the list (so it will be concatinated according to the )
+before = sorted(os.listdir(os.path.join('content', 'drive',
+                                        'MyDrive', 'demo', 'middle', 'input_frames')))
+print(len(before))
+after = sorted(os.listdir(os.path.join('content', 'drive',
+                                       'MyDrive', 'demo', 'middle', 'output_frames')))
+print(len(after))
+
+# loop through 1986 frames
+for i in range(108):
+    # img on the left
+    ip = cv2.imread(os.path.join('content', 'drive', 'MyDrive',
+                                 'demo', 'middle', 'input_frames', before[i]))
+    # print(images[i])
+    # img on the right
+    op = cv2.imread(os.path.join('content', 'drive', 'MyDrive',
+                                 'demo', 'middle', 'output_frames', after[i]))
+    # print(plots[i])
+    # concat 2 image horizontally
+    concat = cv2.hconcat([ip, op])
+    # check output shape
+    print(concat.shape)
+    # save output image
+    cv2.imwrite(os.path.join('content', 'drive', 'MyDrive', 'demo',
+                             'middle', 'compare_frames', after[i]), concat)
