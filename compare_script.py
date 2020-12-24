3b4628cac1588e0ad485c457b20b291c56291e59
Nhan Luong <nhanlt18@gmail.com>
2020-12-24 17:11:21 +0700

    modified code to work with any value of noise sigma

--- compare_script.py
+++ compare_script.py
@@ -9,36 +9,36 @@ def concat_func(ns, num_file):
                                             'MyDrive', 'demo', 'middle', 'input_frames')))
     print(before)
     after = sorted(os.listdir(os.path.join('/content', 'drive',
-                                           'MyDrive', 'demo', 'middle', 'output_frames', 'ns_' + ns)), key=lambda item: int(item.split('_')[-1].split('.')[0]))
+                                           'MyDrive', 'demo', 'middle', 'output_frames', 'ns_' + str(ns)), key=lambda item: int(item.split('_')[-1].split('.')[0]))
     print(after)
 
     for i in range(num_file):
         # img on the left
-        ip = cv2.imread(os.path.join('/content', 'drive', 'MyDrive',
+        ip=cv2.imread(os.path.join('/content', 'drive', 'MyDrive',
                                      'demo', 'middle', 'input_frames', before[i]))
         # print(images[i])
         # img on the right
-        op = cv2.imread(os.path.join('/content', 'drive', 'MyDrive',
-                                     'demo', 'middle', 'output_frames', 'ns_' + ns, after[i]))
+        op=cv2.imread(os.path.join('/content', 'drive', 'MyDrive',
+                                     'demo', 'middle', 'output_frames', 'ns_' + str(ns), after[i]))
         # print(plots[i])
         # concat 2 image horizontally
-        concat = cv2.hconcat([ip, op])
+        concat=cv2.hconcat([ip, op])
         # check output shape
         # save output image
         cv2.imwrite(os.path.join('/content', 'drive', 'MyDrive', 'demo',
-                                 'middle', 'compare_frames', 'ns_' + ns, after[i]), concat)
+                                 'middle', 'compare_frames', 'ns_' + str(ns), after[i]), concat)
 
 
 if __name__ == "__main__":
     # Parse arguments
-    parser = argparse.ArgumentParser(
+    parser=argparse.ArgumentParser(
         description="Denoise a sequence with FastDVDnet")
     parser.add_argument("--noise_sigma", type=int,
                         help='value of noise sigma')
     parser.add_argument("--num_file", type=int,
                         help='number of file to concat')
 
-    argspar = parser.parse_args()
+    argspar=parser.parse_args()
 
     # print(argspar.noise_sigma, argspar.num_file)
 
