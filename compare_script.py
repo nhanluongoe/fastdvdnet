5335bcdf912df13164c80a4179432278df313ac8
Nhan Luong <nhanlt18@gmail.com>
2020-12-24 16:51:58 +0700

    modified code to work with any value of noise sigma

--- compare_script.py
+++ compare_script.py
@@ -1,30 +1,44 @@
 import cv2
 import os
+import argparse
 
-ns = os.environ["NOISESIGMA"]  # noise sigma value
-num_file = os.environ["FILECOUNT"]  # number of frames
 
-# load images into a list and sort the list (so it will be concatinated according to the )
-before = sorted(os.listdir(os.path.join('/content', 'drive',
-                                        'MyDrive', 'demo', 'middle', 'input_frames')))
-print(before)
-after = sorted(os.listdir(os.path.join('/content', 'drive',
-                                       'MyDrive', 'demo', 'middle', 'output_frames', 'ns_' + ns)), key=lambda item: int(item.split('_')[-1].split('.')[0]))
-print(after)
+def concat_func(ns, num_file):
+    # load images into a list and sort the list (so it will be concatinated according to the )
+    before = sorted(os.listdir(os.path.join('/content', 'drive',
+                                            'MyDrive', 'demo', 'middle', 'input_frames')))
+    print(before)
+    after = sorted(os.listdir(os.path.join('/content', 'drive',
+                                           'MyDrive', 'demo', 'middle', 'output_frames', 'ns_' + ns)), key=lambda item: int(item.split('_')[-1].split('.')[0]))
+    print(after)
 
-# loop through 1986 frames
-for i in range(num_file):
-    # img on the left
-    ip = cv2.imread(os.path.join('/content', 'drive', 'MyDrive',
-                                 'demo', 'middle', 'input_frames', before[i]))
-    # print(images[i])
-    # img on the right
-    op = cv2.imread(os.path.join('/content', 'drive', 'MyDrive',
-                                 'demo', 'middle', 'output_frames', 'ns_' + ns, after[i]))
-    # print(plots[i])
-    # concat 2 image horizontally
-    concat = cv2.hconcat([ip, op])
-    # check output shape
-    # save output image
-    cv2.imwrite(os.path.join('/content', 'drive', 'MyDrive', 'demo',
-                             'middle', 'compare_frames', 'ns_' + ns, after[i]), concat)
+    # loop through 1986 frames
+    for i in range(num_file):
+        # img on the left
+        ip = cv2.imread(os.path.join('/content', 'drive', 'MyDrive',
+                                     'demo', 'middle', 'input_frames', before[i]))
+        # print(images[i])
+        # img on the right
+        op = cv2.imread(os.path.join('/content', 'drive', 'MyDrive',
+                                     'demo', 'middle', 'output_frames', 'ns_' + ns, after[i]))
+        # print(plots[i])
+        # concat 2 image horizontally
+        concat = cv2.hconcat([ip, op])
+        # check output shape
+        # save output image
+        cv2.imwrite(os.path.join('/content', 'drive', 'MyDrive', 'demo',
+                                 'middle', 'compare_frames', 'ns_' + ns, after[i]), concat)
+
+
+if __name__ == "__main__":
+    # Parse arguments
+    parser = argparse.ArgumentParser(
+        description="Denoise a sequence with FastDVDnet")
+    parser.add_argument("--noise_sigma", type=int,
+                        help='value of noise sigma')
+    parser.add_argument("--num_file", type=int,
+                        help='number of file to concat')
+
+    argspar = parser.parse_args()
+
+    concat_func(**vars(argspar))
